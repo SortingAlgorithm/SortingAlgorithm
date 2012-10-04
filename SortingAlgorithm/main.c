eb9795c404a98e0ccf5d57e83356c2348f1d1536
lkor <lkor@lkor-VirtualBox>
2012-10-04 14:01:55 +0200

    Commituji novou zmenu v printf

---
+++ SortingAlgorithm/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: lkor
+ *
+ * Created on October 4, 2012, 2:01 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    printf("Ahoj");
+    return (EXIT_SUCCESS);
+}
+
